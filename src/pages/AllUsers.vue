d95e179660cc8f01fca2661b2e96513562725371
Noureddin Hariri <pic.ner101@gmail.com>
2021-09-03 03:21:57 +0100

    route's animation not

--- src/pages/AllUsers.vue
+++ src/pages/AllUsers.vue
@@ -1,4 +1,6 @@
 <template>
+<!-- <transition> needs exactly one child element, to animate routes 
+    so always make sure to wrapp the components -->
   <div class="container">
     <h1>All Users</h1>
     <router-link to="/goals">goals</router-link>
